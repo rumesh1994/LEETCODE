4e768ef1522514113b7581e84b6f575914f0f408
Raksha Umesh <raksha.nalini@gmail.com>
2020-02-02 13:31:59 -0800

    Create Linked List Cycle.py

---
+++ LeetCode/Linked List Cycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        # Keys are the locations ids of elements in linked list to be stored in a dictionary
+        dict = {} 
+        while head:
+            # If id exists, it has a cycle and else add it in the dictionary
+            if id(head) in dict:
+                return True
+            else:
+                dict[id(head)] = True
+            head = head.next
+        return False
+
+        
